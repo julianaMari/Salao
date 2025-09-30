60c8ac3a12ee25921303b3f7cff39b8e556f4ce3
Juliana Cruz <julianamariacruz.jm@gmail.com>
2025-09-30 15:46:54 -0300

    aws configurado

--- ws/index.js
+++ ws/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
+const busboy = require('connect-busboy');
+const busboyBodyParser = require('busboy-body-parser')
 require('./database');
 
 // MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(busboy());
+app.use(busboyBodyParser());
 app.use(cors());
 
 
--- ws/src/routes/servico.routes.js
+++ ws/src/routes/servico.routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const Busboy = require('busboy');
+const aws = require('../services/aws')
 const Salao = require('../models/salao');
 const Servico = require('../models/servico');
 
 router.post('/', async (req, res) => {
     try{
+        let busboy = new Busboy({ headers: req.header });
 
     } catch (err) {
         res.json({ error: true, message: err.message })
