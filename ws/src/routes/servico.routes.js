396581f5908548b9d81f6bc9225279cc67f9090d
Juliana Cruz <julianamariacruz.jm@gmail.com>
2025-10-27 11:34:57 -0300

    Atualizando estados de excluido

--- ws/src/routes/servico.routes.js
+++ ws/src/routes/servico.routes.js
@@ -137,4 +137,20 @@ router.post('/delete-arquivo', async (req, res) => {
         }
 })
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Servico.findByIdAndUpdate(id, { status: 'E' });
+        
+        res.json({ error: false });
+
+    } catch (err) {
+        console.error('Erro na rota POST /servico:', err.message); 
+            res.status(500).json({ 
+                error: true, 
+                message: err.message 
+            });
+        }
+})
+
 module.exports = router;
